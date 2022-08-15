f3fd83fc94981b55e611cc8320cf8770165f918d
NAJI2329 <be1512t6@gmail.com>
2022-08-15 10:18:54 +0900

    update OICL

--- backend/routes/api/technicianlists.js
+++ backend/routes/api/technicianlists.js
@@ -49,4 +49,33 @@ router.get('/', async (req, res) => {
   }
 });
 
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get('/getTechnicianById/:id', async ({ params: { id } }, res) => {
+  try {
+    const technicianlist = await Technicianlist.findOne({
+      _id: id
+    });
+
+    if (!technicianlist)
+      return res.status(400).json({ msg: 'Profile not found' });
+
+    return res.json(technicianlist);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ msg: 'Server error' });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const technicianlists = await Technicianlist.find();
+    res.json(technicianlists);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
